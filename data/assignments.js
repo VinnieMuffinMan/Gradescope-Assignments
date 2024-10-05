6d0d1e4bcb189e42fa02d8bc75085cbbeafdaeb1
planit <planit@tanjeffreyz.github.io>
2024-10-05 03:40:06 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,123 @@
-assignments = {};
+assignments = {
+  "CSE 110": [
+    {
+      "title": "Lab 1 Video",
+      "course": "CSE 110",
+      "dueDate": "2024-10-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/883395/assignments/5075505/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Topic and interview questions",
+      "course": "CSE 110",
+      "dueDate": "2024-10-07T19:00:00",
+      "link": "https://www.gradescope.com/courses/883395/assignments/5076746/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Clicker replacement 10/2",
+      "course": "CSE 110",
+      "dueDate": "2024-10-04T17:58:00",
+      "link": "https://www.gradescope.com/courses/883395/assignments/5092212/submissions/274424970",
+      "submitted": true
+    }
+  ],
+  "CSE107_FA24_A00": [
+    {
+      "title": "Problem Set 0",
+      "course": "CSE107_FA24_A00",
+      "dueDate": "2024-10-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/875836/assignments/5022393/submissions/274425517",
+      "submitted": true
+    },
+    {
+      "title": "Problem Set 1",
+      "course": "CSE107_FA24_A00",
+      "dueDate": "2024-10-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/875836",
+      "submitted": false
+    },
+    {
+      "title": "Problem Set 2",
+      "course": "CSE107_FA24_A00",
+      "dueDate": "2024-10-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/875836",
+      "submitted": false
+    }
+  ],
+  "CSE156_FA24_A00": [],
+  "MATH140A_FA24_A00": [
+    {
+      "title": "Homework 1",
+      "course": "MATH140A_FA24_A00",
+      "dueDate": "2024-10-04T21:00:00",
+      "link": "https://www.gradescope.com/courses/886078/assignments/5079895/submissions/274576883",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "MATH140A_FA24_A00",
+      "dueDate": "2024-10-11T21:00:00",
+      "link": "https://www.gradescope.com/courses/886078",
+      "submitted": false
+    },
+    {
+      "title": "Homework 3",
+      "course": "MATH140A_FA24_A00",
+      "dueDate": "2024-10-18T21:00:00",
+      "link": "https://www.gradescope.com/courses/886078",
+      "submitted": false
+    },
+    {
+      "title": "Homework 4",
+      "course": "MATH140A_FA24_A00",
+      "dueDate": "2024-10-28T21:00:00",
+      "link": "https://www.gradescope.com/courses/886078",
+      "submitted": false
+    },
+    {
+      "title": "Homework 5",
+      "course": "MATH140A_FA24_A00",
+      "dueDate": "2024-11-01T21:00:00",
+      "link": "https://www.gradescope.com/courses/886078",
+      "submitted": false
+    },
+    {
+      "title": "Homework 6",
+      "course": "MATH140A_FA24_A00",
+      "dueDate": "2024-11-08T21:00:00",
+      "link": "https://www.gradescope.com/courses/886078",
+      "submitted": false
+    },
+    {
+      "title": "Homework 7",
+      "course": "MATH140A_FA24_A00",
+      "dueDate": "2024-11-15T21:00:00",
+      "link": "https://www.gradescope.com/courses/886078",
+      "submitted": false
+    },
+    {
+      "title": "Homework 8",
+      "course": "MATH140A_FA24_A00",
+      "dueDate": "2024-11-29T21:00:00",
+      "link": "https://www.gradescope.com/courses/886078",
+      "submitted": false
+    },
+    {
+      "title": "Homework 9",
+      "course": "MATH140A_FA24_A00",
+      "dueDate": "2024-12-06T21:00:00",
+      "link": "https://www.gradescope.com/courses/886078",
+      "submitted": false
+    }
+  ],
+  "MATH187A_FA24_A00": [
+    {
+      "title": "Mathematical Autobiography",
+      "course": "MATH187A_FA24_A00",
+      "dueDate": "2024-10-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/875374/assignments/5042249/submissions/274645615",
+      "submitted": true
+    }
+  ]
+};
