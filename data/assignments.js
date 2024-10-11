1633f1cfb973e20abfa4d0529bff8793c942cd43
planit <planit@tanjeffreyz.github.io>
2024-10-11 20:37:54 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -64,6 +64,13 @@ assignments = {
       "dueDate": "2024-10-13T23:59:00",
       "link": "https://www.gradescope.com/courses/875836",
       "submitted": false
+    },
+    {
+      "title": "Problem Set 3",
+      "course": "CSE107_FA24_A00",
+      "dueDate": "2024-10-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/875836",
+      "submitted": false
     }
   ],
   "CSE156_FA24_A00": [
