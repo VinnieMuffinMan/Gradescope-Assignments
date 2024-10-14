648d55c507bdcb9124bfddd38548ad0b6d38bdd1
planit <planit@tanjeffreyz.github.io>
2024-10-14 20:36:41 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -55,6 +55,13 @@ assignments = {
       "dueDate": "2024-10-18T23:59:00",
       "link": "https://www.gradescope.com/courses/883395/assignments/5147398/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Lab 3 Submission",
+      "course": "CSE 110",
+      "dueDate": "2024-10-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/883395/assignments/5155437/submissions/new",
+      "submitted": false
     }
   ],
   "CSE107_FA24_A00": [
