92250c8f9b07aaccd5c09fd12d4bc04916a662f6
planit <planit@tanjeffreyz.github.io>
2024-10-18 01:58:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -98,14 +98,14 @@ assignments = {
     {
       "title": "PA1 Report",
       "course": "CSE156_FA24_A00",
-      "dueDate": "2024-10-18T23:59:00",
+      "dueDate": "2024-10-20T23:59:00",
       "link": "https://www.gradescope.com/courses/872298",
       "submitted": false
     },
     {
       "title": "PA1 Code",
       "course": "CSE156_FA24_A00",
-      "dueDate": "2024-10-18T23:59:00",
+      "dueDate": "2024-10-20T23:59:00",
       "link": "https://www.gradescope.com/courses/872298",
       "submitted": false
     }
