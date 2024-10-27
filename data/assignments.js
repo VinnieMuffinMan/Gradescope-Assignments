41ae188903872151f24c35d7e4f4db1194f36786
planit <planit@tanjeffreyz.github.io>
2024-10-27 22:37:34 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -157,6 +157,13 @@ assignments = {
       "dueDate": "2024-10-20T23:59:00",
       "link": "https://www.gradescope.com/courses/872298/assignments/5142418/submissions/280094551",
       "submitted": true
+    },
+    {
+      "title": "PA2 Code",
+      "course": "CSE156_FA24_A00",
+      "dueDate": "2024-11-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/872298",
+      "submitted": false
     }
   ],
   "MATH140A_FA24_A00": [
