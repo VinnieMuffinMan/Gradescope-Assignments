5609719a5a8667d52c1f4b54035e6880e6aac0e3
planit <planit@tanjeffreyz.github.io>
2025-01-06 18:42:28 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,11 @@
 assignments = {
-  "CSE150A_WI25_A00": []
+  "CSE150A_WI25_A00": [
+    {
+      "title": "Week 1: Wed Lecture Participation",
+      "course": "CSE150A_WI25_A00",
+      "dueDate": "2025-11-10T09:00:00",
+      "link": "https://www.gradescope.com/courses/936983",
+      "submitted": false
+    }
+  ]
 };
