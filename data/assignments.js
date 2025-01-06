ac02c839c20c6781803f8a8cf711ed186ced4663
planit <planit@tanjeffreyz.github.io>
2025-01-06 20:35:45 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -7,5 +7,14 @@ assignments = {
       "link": "https://www.gradescope.com/courses/936983",
       "submitted": false
     }
+  ],
+  "MATH180B_WI25": [
+    {
+      "title": "Homework1",
+      "course": "MATH180B_WI25",
+      "dueDate": "2025-01-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/939995",
+      "submitted": false
+    }
   ]
 };
