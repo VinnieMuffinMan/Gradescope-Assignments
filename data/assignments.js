046f60b74bdd401aa5e8dd499f5ca23f36d4f66f
planit <planit@tanjeffreyz.github.io>
2025-01-07 05:37:08 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -6,6 +6,13 @@ assignments = {
       "dueDate": "2025-01-02T08:00:00",
       "link": "https://www.gradescope.com/courses/936983",
       "submitted": false
+    },
+    {
+      "title": "Week 1: Mon Discussion Participation PDF",
+      "course": "CSE150A_WI25_A00",
+      "dueDate": "2025-01-08T08:00:00",
+      "link": "https://www.gradescope.com/courses/936983",
+      "submitted": false
     }
   ],
   "MATH180B_WI25": [
