cefaaad792745fbfd4214e858c80b99a74819028
planit <planit@tanjeffreyz.github.io>
2025-01-24 04:38:23 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -85,6 +85,13 @@ assignments = {
       "dueDate": "2025-01-26T23:59:00",
       "link": "https://www.gradescope.com/courses/936983",
       "submitted": false
+    },
+    {
+      "title": "Group Project Milestone 1: Abstract, Data and Group Members",
+      "course": "CSE150A_WI25_A00",
+      "dueDate": "2025-02-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/936983/assignments/5663023/submissions/new",
+      "submitted": false
     }
   ],
   "CSE202_WI25_A00": [
