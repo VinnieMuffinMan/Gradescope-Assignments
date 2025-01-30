a5aad1eaf2b23b05587089b3f032d6bc8f1c850b
planit <planit@tanjeffreyz.github.io>
2025-01-30 06:41:21 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -125,8 +125,8 @@ assignments = {
       "title": "Homework 1: Coding Assignment (ipynb ONLY! NO PDFS!!!! PDF = 0 points)",
       "course": "CSE150A_WI25_A00",
       "dueDate": "2025-01-29T23:59:00",
-      "link": "https://www.gradescope.com/courses/936983",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/936983/assignments/5675065/submissions/303349318",
+      "submitted": true
     },
     {
       "title": "HW6: Data Processing AG Example",
