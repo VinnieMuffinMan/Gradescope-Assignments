5dba79a673d0a7260cfeaeaba7b1ff0ffd632dbf
planit <planit@tanjeffreyz.github.io>
2025-02-05 21:35:59 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -220,6 +220,13 @@ assignments = {
       "dueDate": "2025-02-04T23:59:00",
       "link": "https://www.gradescope.com/courses/935255",
       "submitted": false
+    },
+    {
+      "title": "Homework 3",
+      "course": "CSE202_WI25_A00",
+      "dueDate": "2025-02-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/935255",
+      "submitted": false
     }
   ],
   "MATH180B_WI25": [
