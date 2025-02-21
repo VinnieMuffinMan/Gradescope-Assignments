6c89a3948c9933da4c6a80c53c0d67b7c9b83745
planit <planit@tanjeffreyz.github.io>
2025-02-21 22:36:59 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -318,6 +318,13 @@ assignments = {
       "dueDate": "2025-02-13T23:59:00",
       "link": "https://www.gradescope.com/courses/935255",
       "submitted": false
+    },
+    {
+      "title": "Project Algorithm and Analysis",
+      "course": "CSE202_WI25_A00",
+      "dueDate": "2025-03-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/935255",
+      "submitted": false
     }
   ]
 };
