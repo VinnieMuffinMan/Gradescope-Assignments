514540a8cd98a91ce9e91e4c10a8336a9d015522
planit <planit@tanjeffreyz.github.io>
2025-03-05 20:38:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -413,7 +413,7 @@ assignments = {
     {
       "title": "Project Implementation",
       "course": "CSE202_WI25_A00",
-      "dueDate": "2025-03-04T23:59:00",
+      "dueDate": "2025-03-14T23:59:00",
       "link": "https://www.gradescope.com/courses/935255",
       "submitted": false
     }
