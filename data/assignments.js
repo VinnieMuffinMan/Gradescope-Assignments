a18323f50179714ab73cb3ba9396e5e0b77fdbbc
planit <planit@tanjeffreyz.github.io>
2025-03-07 15:37:11 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -293,8 +293,8 @@ assignments = {
       "title": "Week 9: Wed Lecture Participation PDF",
       "course": "CSE150A_WI25_A00",
       "dueDate": "2025-03-06T08:00:00",
-      "link": "https://www.gradescope.com/courses/936983",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/936983/assignments/5536883/submissions/315286875",
+      "submitted": true
     },
     {
       "title": "Homework 1: PDF Worksheet",
