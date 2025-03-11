37a27829ed744d598a888523f95e4e8f4e1d1a34
planit <planit@tanjeffreyz.github.io>
2025-03-11 07:34:53 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -83,6 +83,13 @@ assignments = {
       "dueDate": "2025-03-07T23:59:00",
       "link": "https://www.gradescope.com/courses/951193/assignments/5880181/submissions/315613863",
       "submitted": true
+    },
+    {
+      "title": "handout 3-11",
+      "course": "CSE130_WI25_A00",
+      "dueDate": "2025-03-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/951193",
+      "submitted": false
     }
   ],
   "CSE150A_WI25_A00": [
