a019d3884dd109b366e82bee7f4f95bf04e36c55
planit <planit@tanjeffreyz.github.io>
2025-04-03 21:35:38 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,11 @@
 assignments = {
-  "CSE120_SP25_A00": []
+  "CSE120_SP25_A00": [
+    {
+      "title": "Homework 1",
+      "course": "CSE120_SP25_A00",
+      "dueDate": "2025-04-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/1008825",
+      "submitted": false
+    }
+  ]
 };
