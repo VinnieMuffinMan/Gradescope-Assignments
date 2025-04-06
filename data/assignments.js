4600d2449186e480e16c5ea5996af2a70bb90813
planit <planit@tanjeffreyz.github.io>
2025-04-06 03:13:48 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -22,8 +22,8 @@ assignments = {
       "title": "Homework 1",
       "course": "MATH180B_SP25_A00",
       "dueDate": "2025-04-05T23:00:00",
-      "link": "https://www.gradescope.com/courses/1014559",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/1014559/assignments/6020143/submissions/322819763",
+      "submitted": true
     }
   ]
 };
