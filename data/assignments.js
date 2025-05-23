7c69094ef2ff0dcb7fbc15e4dd38909711f390d0
planit <planit@tanjeffreyz.github.io>
2025-05-23 05:52:05 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -20,6 +20,34 @@ assignments = {
       "dueDate": "2025-04-24T23:59:00",
       "link": "https://www.gradescope.com/courses/1016806/assignments/6134547/submissions/328135380",
       "submitted": true
+    },
+    {
+      "title": "hw4",
+      "course": "CSE 151A",
+      "dueDate": "2025-05-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/1016806/assignments/6158190/submissions/330558799",
+      "submitted": true
+    },
+    {
+      "title": "hw5",
+      "course": "CSE 151A",
+      "dueDate": "2025-05-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/1016806/assignments/6180544/submissions/332274972",
+      "submitted": true
+    },
+    {
+      "title": "Hw6",
+      "course": "CSE 151A",
+      "dueDate": "2025-05-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/1016806/assignments/6240522/submissions/333591431",
+      "submitted": true
+    },
+    {
+      "title": "hw 7",
+      "course": "CSE 151A",
+      "dueDate": "2025-05-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/1016806",
+      "submitted": false
     }
   ],
   "CSE120_SP25_A00": [
@@ -41,15 +69,15 @@ assignments = {
       "title": "Project 1",
       "course": "CSE120_SP25_A00",
       "dueDate": "2025-04-29T23:59:00",
-      "link": "https://www.gradescope.com/courses/1008825",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/1008825/assignments/6064901/submissions/329355983",
+      "submitted": true
     },
     {
       "title": "Homework 2",
       "course": "CSE120_SP25_A00",
       "dueDate": "2025-04-25T23:59:00",
-      "link": "https://www.gradescope.com/courses/1008825",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/1008825/assignments/6093818/submissions/328989329",
+      "submitted": true
     },
     {
       "title": "In-Class Poll #2",
@@ -63,6 +91,62 @@ assignments = {
       "course": "CSE120_SP25_A00",
       "dueDate": "2025-04-24T15:54:00",
       "link": "https://www.gradescope.com/courses/1008825/assignments/6135929/submissions/328342692",
+      "submitted": true
+    },
+    {
+      "title": "Project 2",
+      "course": "CSE120_SP25_A00",
+      "dueDate": "2025-05-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/1008825/assignments/6156893/submissions/333747086",
+      "submitted": true
+    },
+    {
+      "title": "Project 1 Testing",
+      "course": "CSE120_SP25_A00",
+      "dueDate": "2025-06-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/1008825",
+      "submitted": false
+    },
+    {
+      "title": "Homework #3",
+      "course": "CSE120_SP25_A00",
+      "dueDate": "2025-05-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/1008825/assignments/6196388/submissions/334323245",
+      "submitted": true
+    },
+    {
+      "title": "In-Class Poll #4",
+      "course": "CSE120_SP25_A00",
+      "dueDate": "2025-05-08T16:00:00",
+      "link": "https://www.gradescope.com/courses/1008825/assignments/6212874/submissions/332186837",
+      "submitted": true
+    },
+    {
+      "title": "In-Class Poll #5",
+      "course": "CSE120_SP25_A00",
+      "dueDate": "2025-05-13T16:06:00",
+      "link": "https://www.gradescope.com/courses/1008825/assignments/6230517/submissions/333096781",
+      "submitted": true
+    },
+    {
+      "title": "Project 3",
+      "course": "CSE120_SP25_A00",
+      "dueDate": "2025-06-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/1008825",
+      "submitted": false
+    },
+    {
+      "title": "Project 2 Testing",
+      "course": "CSE120_SP25_A00",
+      "dueDate": "2025-06-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/1008825",
+      "submitted": false
+    },
+    {
+      "title": "In-Class Poll #6",
+      "course": "CSE120_SP25_A00",
+      "dueDate": "2025-05-22T16:31:00",
+      "link": "https://www.gradescope.com/courses/1008825/assignments/6271888/submissions/334584636",
       "submitted": true
     }
   ],
@@ -78,6 +162,34 @@ assignments = {
       "title": "PA2: 2048",
       "course": "CSE150B_SP25",
       "dueDate": "2025-04-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/1014520/assignments/6073668/submissions/329057519",
+      "submitted": true
+    },
+    {
+      "title": "PA3: Blackjack",
+      "course": "CSE150B_SP25",
+      "dueDate": "2025-05-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/1014520/assignments/6128005/submissions/332862770",
+      "submitted": true
+    },
+    {
+      "title": "PA4: Gomoku",
+      "course": "CSE150B_SP25",
+      "dueDate": "2025-05-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/1014520",
+      "submitted": false
+    },
+    {
+      "title": "Gomoku Competition",
+      "course": "CSE150B_SP25",
+      "dueDate": "2025-06-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/1014520",
+      "submitted": false
+    },
+    {
+      "title": "PA5: Sudoku",
+      "course": "CSE150B_SP25",
+      "dueDate": "2025-06-08T23:59:00",
       "link": "https://www.gradescope.com/courses/1014520",
       "submitted": false
     }
@@ -103,6 +215,27 @@ assignments = {
       "dueDate": "2025-04-19T22:00:00",
       "link": "https://www.gradescope.com/courses/1014559/assignments/6082339/submissions/326944734",
       "submitted": true
+    },
+    {
+      "title": "Homework 4",
+      "course": "MATH180B_SP25_A00",
+      "dueDate": "2025-05-03T22:00:00",
+      "link": "https://www.gradescope.com/courses/1014559/assignments/6159056/submissions/331021479",
+      "submitted": true
+    },
+    {
+      "title": "Homework 5",
+      "course": "MATH180B_SP25_A00",
+      "dueDate": "2025-05-10T22:00:00",
+      "link": "https://www.gradescope.com/courses/1014559/assignments/6184073/submissions/332160808",
+      "submitted": true
+    },
+    {
+      "title": "Homework 6",
+      "course": "MATH180B_SP25_A00",
+      "dueDate": "2025-05-17T22:00:00",
+      "link": "https://www.gradescope.com/courses/1014559/assignments/6223987/submissions/333856128",
+      "submitted": true
     }
   ],
   "Math 160B": [
@@ -118,6 +251,36 @@ assignments = {
       "course": "Math 160B",
       "dueDate": "2025-04-22T23:59:00",
       "link": "https://www.gradescope.com/courses/1013103/assignments/6073225/submissions/327786448",
+      "submitted": true
+    },
+    {
+      "title": "Problem Set 3",
+      "course": "Math 160B",
+      "dueDate": "2025-05-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/1013103/assignments/6160625/submissions/331495767",
+      "submitted": true
+    },
+    {
+      "title": "Problem Set 4",
+      "course": "Math 160B",
+      "dueDate": "2025-05-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/1013103",
+      "submitted": false
+    }
+  ],
+  "SDCTF 2025": [
+    {
+      "title": "Test assignment",
+      "course": "SDCTF 2025",
+      "dueDate": "2025-05-18T16:00:00",
+      "link": "https://www.gradescope.com/courses/1037463",
+      "submitted": false
+    },
+    {
+      "title": "Numerical Estimation Activity",
+      "course": "SDCTF 2025",
+      "dueDate": "2025-05-11T16:00:00",
+      "link": "https://www.gradescope.com/courses/1037463/assignments/6219980/submissions/332654922",
       "submitted": true
     }
   ]
